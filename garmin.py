2cf1b556fcfaa7201dbb7787f8e0a0007656ed35
Adam Parkin <adam.parkin@zapier.com>
2022-10-19 19:20:03 -0700

    Change to calculate based on yesterday cutoff

--- garmin.py
+++ garmin.py
@@ -5,59 +5,47 @@ import json
 import logging
 import os
 import sys
-from datetime import date, datetime, timedelta
+from datetime import date, timedelta
 from random import random
 from time import sleep
 
 import requests
 from garminconnect import Garmin
 
-from db import StepEntry, db_session, get_steps_per_day_from_db
-from inputs import date_picker, number_picker, yes_no
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
+DAYS_IN_PERIOD = 7
 TARGET_STEP_GOAL = 11_000
 API = None
 
 
-def number_of_days_picker():
-    return number_picker("How many days in period?", 7)
+def day_count(day):
+    day = day.isoformat()
+    logging.info(f"Requesting steps for {day}")
+    steps = API.get_steps_data(day)
+    # throttle a little bit
+    sleep(random())
+
+    total_steps_for_day = sum(x["steps"] for x in steps)
+    return {
+        "day": day,
+        "steps": total_steps_for_day,
+        "goal_met": total_steps_for_day > TARGET_STEP_GOAL,
+    }
 
 
-def get_from_garmin(day: date) -> StepEntry:
-    with db_session() as session:
-        orig_day = day
-        day = day.isoformat()
-        logging.info(f"Requesting steps for {day}")
-        steps = API.get_steps_data(day)
-        # throttle a little bit
-        sleep(random())
-
-        total_steps_for_day = sum(x["steps"] for x in steps)
-        entry = StepEntry(
-            day=orig_day,
-            step_count=total_steps_for_day,
-            goal_met=total_steps_for_day > TARGET_STEP_GOAL,
-        )
-
-        session.add(entry)
-        session.commit()
-
-    return entry
-
-
-def summarize(start_date, end_date, data, days):
-    total_steps = sum(x.step_count for x in data)
-    goal_days = sum(x.goal_met for x in data)
+def summarize(start_date, end_date, data):
+    total_steps = sum(x["steps"] for x in data)
+    goal_days = sum(x["goal_met"] for x in data)
     return {
         "start_date": f"{start_date:%-d-%b-%Y}",
         "end_date": f"{end_date:%-d-%b-%Y}",
         "step_total": f"{total_steps:,}",
-        "step_average": f"{total_steps // days:,}",
+        "step_average": f"{total_steps // DAYS_IN_PERIOD:,}",
         "num_days_goal_met": goal_days,
-        "days_in_period": days,
-        "percent_goal_met": f"{(goal_days/days* 100):.1f}%",
+        "days_in_period": DAYS_IN_PERIOD,
+        "percent_goal_met": f"{(goal_days/DAYS_IN_PERIOD* 100):.1f}%",
         "username": "aparkin",
     }
 
@@ -66,64 +54,61 @@ def initialize_api():
     global API
 
     logging.info("Logging in with garmin...")
-    API = Garmin(os.getenv("GARMIN_EMAIL"), os.getenv("GARMIN_PASSWORD"))
+    API = Garmin(
+        os.getenv("GARMIN_EMAIL"),
+        os.getenv("GARMIN_PASSWORD"),
+        session_data=read_session(),
+    )
     if not API.login():
         logging.exception("failed to log in, aborting")
         exit(1)
     logging.info("Logged in")
 
 
+def read_session():
+    restored_session = None
+    with contextlib.suppress(Exception):
+        with open("session_data.json", "r") as fobj:
+            restored_session = json.loads(fobj.readlines()[0])
+    return restored_session
+
+
+def write_session():
+    # Write out session data for next run
+    session_data = json.dumps(API.session_data)
+    with open("session_data.json", "w") as fobj:
+        fobj.write(session_data)
+
+
 @contextlib.contextmanager
 def garmin_api():
     initialize_api()
-    yield
+    try:
+        yield
+    finally:
+        write_session()
 
 
 def post_to_zap(data):
-    if not yes_no("Post to Step Challenge channel?", 0):
-        return
-
     url = os.getenv("ZAPIER_WEBHOOK_URL", None)
-
     headers = {"Content-type": "application/json"}
-    data = json.dumps(data)
     logging.info(f"Posting to zap -- {url} - data {json.dumps(data)}")
     result = requests.post(url, data=data, headers=headers)
     logging.info(f"Response: {result.status_code} - {result.json()}")
 
 
-def process_range(start_date: date, days: int):
-    dates = {start_date + timedelta(days=i): None for i in range(days)}
-
-    # grab any existing values from the DB
-    for day in dates:
-        dates[day] = get_steps_per_day_from_db(day)
-
-    # any unknown values read from Garmin
-    if remaining_days := [k for k, count in dates.items() if not count]:
-        with garmin_api():
-            for day in remaining_days:
-                dates[day] = get_from_garmin(day)
-
-    return dates.values()
-
-
-def get_end_date():
-    today = datetime.now().date()
-    if yes_no(f"Use today ({today}) as end date? ", default=1):
-        return today
-    return date_picker()
-
-
 def main():
     # end date is yesterday
-    end_date = get_end_date() - timedelta(days=1)
-    days = number_of_days_picker()
-    start_date = end_date - timedelta(days=days - 1)
+    end_date = date.today() - timedelta(days=1)
+    start_date = end_date - timedelta(days=DAYS_IN_PERIOD - 1)
 
-    result = process_range(start_date, days)
+    with garmin_api():
+        # calculate steps between the two dates
+        result = [
+            day_count(start_date + timedelta(days=i)) for i in range(DAYS_IN_PERIOD)
+        ]
 
-    data = summarize(start_date, end_date, result, days)
+    data = summarize(start_date, end_date, result)
     logging.info(f"Data: {data}")
     post_to_zap(data)
 
