cba26d7fd3a7ee0626ef8e7fe6b74cf157f916f8
Adam Parkin <adam.parkin@zapier.com>
2023-01-31 13:10:40 -0800

    Add misc queries file

---
+++ misc_queries.sql
@@ -0,0 +1,14 @@
+-- average BMI, weight by month
+SELECT
+    date_part('month', day) || '/' || date_part('year', day) as month_year,
+    avg(bmi) as avg_bmi,
+    avg(weight_grams) as avg_weight_grams,
+    avg(weight_grams) * 0.00220462 as avg_weight_pounds
+FROM
+    daystats
+GROUP BY
+    date_part('month', day),
+    date_part('year', day)
+ORDER BY
+    year,
+    month;
