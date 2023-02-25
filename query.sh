1df1ff329ceb4ad7e154f2740242e29c4bf09e06
Adam Parkin <adam.parkin@zapier.com>
2023-02-25 10:58:00 -0800

    Refactor query.sh to do one psql call

--- query.sh
+++ query.sh
@@ -5,13 +5,14 @@ LAST_SUN=`python last_day_for_weekly_post.py`
 echo "From $LAST_SUN"
 
 echo "============================================="
+echo ""
 
-psql $CONN_STR -t -c "SELECT sum(step_count) || ' steps so far' FROM daystats WHERE day > '$LAST_SUN';"
+QUERY="
+SELECT sum(step_count) || ' steps so far' FROM daystats WHERE day > '$LAST_SUN';
+SELECT '==========================';
+SELECT TO_CHAR(day::date, 'mm/dd') || ' - ' || TO_CHAR(step_count, 'fm999G999') || ' :check:' FROM daystats WHERE day > '$LAST_SUN';
+SELECT '==========================';
+SELECT day || ',' || (weight_grams * 0.00220462) FROM daystats WHERE day >= '$LAST_SUN';
+"
 
-echo "============================================="
-
-psql $CONN_STR -t -c "SELECT TO_CHAR(day::date, 'mm/dd') || ' - ' || TO_CHAR(step_count, 'fm999G999') || ' :check:' FROM daystats WHERE day > '$LAST_SUN';"
-
-echo "============================================="
-
-psql $CONN_STR -t -c "SELECT day || ',' || (weight_grams * 0.00220462) FROM daystats WHERE day >= '$LAST_SUN';"
+psql $CONN_STR -t -c "$QUERY"
