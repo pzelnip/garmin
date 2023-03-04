151478fbcac2d7cb474b6318e2ac6a30f0e482cd
Adam Parkin <adam.parkin@zapier.com>
2023-03-04 14:01:02 -0800

    Read hourly step data from json file

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -5,13 +7,15 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def step_progress():
+    with open("dailysteps.json", "r") as fobj:
+        data = json.load(fobj)
+
     env = Environment(
         loader=FileSystemLoader("."),
         autoescape=select_autoescape(["html", "xml"]),
     )
-    template = env.get_template("graph.jinja2")
-    return template.render(hourly_step_data=[0, 2000, 4654, 5800, 6000])
+    return env.get_template("graph.jinja2").render(hourly_step_data=data["step_data"])
 
 
 if __name__ == "__main__":
